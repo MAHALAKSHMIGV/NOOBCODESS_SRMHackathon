0e071231f4bae596308ce35251341e91c72091f9
MAHALAKSHMIGV <71757366+MAHALAKSHMIGV@users.noreply.github.com>
2022-04-03 09:47:48 +0530

    Add files via upload

---
+++ detector.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+from PIL import Image
+import pickle
+import sqlite3
+detector=
+cv2.CascadeClassifier('haarcascade_frontalface_default.xml'
+)
+rec=cv2.face.LBPHFaceRecognizer_create();
+rec.read("trainner/trainningData.yml")
+id=0
+def getProfile(id):
+    conn=sqlite3.connect("FaceBase.db")
+    cmd="SELECT * FROM People WHERE ID="+str(id)
+    cursor=conn.execute(cmd)
+    profile=None
+    for row in cursor:
+        profile=row
+    conn.close()
+    return profile
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+while(True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        id,conf=rec.predict(gray[y:y+h,x:x+w])
+        profile=getProfile(id)
+        if(profile!=None):
+cv2.putText(img,"Name:"+str(profile[1]),(x,y+h+30),font,1,(
+255,255,255),2)#Draw the text
+cv2.putText(img,str(profile[2]),(x,y+h+60),font,1,(255,255,2
+55),2)#Draw the text
+cv2.imshow('frame',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
